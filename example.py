f4a8e892125daa813225cfd68140e5c1fe043b2f
Adrien Osakwe <adrien.osakwe@mail.mcgill.ca>
2024-07-12 13:55:06 -0400

    Created plot function in script

--- example.py
+++ example.py
@@ -1,8 +1,19 @@
 #!/bin/python3
 
-name = input("Enter name: ")
+import numpy as np
+import matplotlib.pyplot as plt
+#name = input("Enter name: ")
 
-print("Hello " + name)
+
+x = [i for i in range(25)]
+x = np.array(x)
+
+y = x**2
+
+plt.plot(x,y)
+plt.savefig("plot_example.png")
+
+#print("Hello " + name)
 
 
 #Simple comment
